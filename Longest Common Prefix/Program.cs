4bbd3804fb4f4318cd1263109564770118bc8c0a
ShadowGreg <shadow-greg@yandex.ru>
2023-07-18 20:50:46 +0300

    add new task

--- Longest Common Prefix/Program.cs
+++ Longest Common Prefix/Program.cs
@@ -1,3 +1,50 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using System.Data;
 
-Console.WriteLine("Hello, World!");
+public class Program {
+    private static Solution _solution;
+
+    public static void Main() {
+        _solution = new Solution();
+
+        string[] thomthing = new[] { "flower", "flow", "flight" }; //fl
+        Console.WriteLine(_solution.LongestCommonPrefix(thomthing));
+
+        thomthing = new[] { "dog", "racecar", "car" }; //""
+        Console.WriteLine(_solution.LongestCommonPrefix(thomthing));
+
+        thomthing = new[] { "flower", "flower", "flower", "flower" }; //flower
+        Console.WriteLine(_solution.LongestCommonPrefix(thomthing));
+
+        thomthing = new[] { "c", "acc", "ccc" }; // 
+        Console.WriteLine(_solution.LongestCommonPrefix(thomthing));
+    }
+}
+
+public class Solution {
+    public string LongestCommonPrefix(string[] strs) {
+        if (strs.Length == 1)
+            return strs[0];
+
+        string prefixString = "";
+
+        if (strs == null || strs.Length == 0)
+            return prefixString;
+
+        for (int i = 0; i < strs[0].Length; i++) {
+            foreach (string str in strs) {
+                if (i > str.Length - 1)
+                    return prefixString;
+
+
+                if (strs[0][i] != str[i])
+                    return prefixString;
+            }
+
+
+            prefixString += strs[0][i];
+        }
+
+
+        return prefixString;
+    }
+}
---
+++ String to Integer/Program.cs
@@ -0,0 +1,50 @@
+﻿using System.Data;
+
+public class Program {
+    private static Solution _solution;
+
+    public static void Main() {
+        _solution = new Solution();
+
+        string thomthing = "42"; //42
+        Console.WriteLine(_solution.MyAtoi(thomthing));
+
+        thomthing = "   -42"; //-42
+        Console.WriteLine(_solution.MyAtoi(thomthing));
+
+        thomthing = "4193 with words"; //4193
+        Console.WriteLine(_solution.MyAtoi(thomthing));
+    }
+}
+
+public class Solution {
+    public int MyAtoi(string s) {
+        int index = 0;
+        int sign = 1;
+        int result = 0;
+        // Ignore leading whitespace
+        while (index < s.Length && s[index] == ' ') {
+            index++;
+        }
+
+        // Check for sign
+        if (index < s.Length && (s[index] == '+' || s[index] == '-')) {
+            sign = (s[index] == '-') ? -1 : 1;
+            index++;
+        }
+
+        // Convert digits to integer
+        while (index < s.Length && char.IsDigit(s[index])) {
+            int digit = s[index] - '0';
+            // Check for overflow
+            if (result > int.MaxValue / 10 || (result == int.MaxValue / 10 && digit > int.MaxValue % 10)) {
+                return (sign == -1) ? int.MinValue : int.MaxValue;
+            }
+
+            result = result * 10 + digit;
+            index++;
+        }
+
+        return result * sign;
+    }
+}
