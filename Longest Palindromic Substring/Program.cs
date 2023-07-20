70b2355d25064d1bef8ac1b7c49be53f003b3e38
ShadowGreg <shadow-greg@yandex.ru>
2023-07-20 14:07:05 +0300

    решение работает но не надлинных строках Longest Palindromic Substring

---
+++ Longest Palindromic Substring/Program.cs
@@ -0,0 +1,57 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+public class Program {
+    private static Solution _solution;
+
+    public static void Main() {
+        _solution = new Solution();
+
+        string text = "babad"; //bab
+        // Console.WriteLine(_solution.LongestPalindrome(text));
+        text = "cbbd"; //bb
+        // Console.WriteLine(_solution.LongestPalindrome(text));
+        // text = "b"; //b
+        // Console.WriteLine(_solution.LongestPalindrome(text));
+        // text = "1aba1"; //aba
+        // Console.WriteLine(_solution.LongestPalindrome(text));
+        // text = "111bab"; //111
+        // Console.WriteLine(_solution.LongestPalindrome(text));
+        // text = "aacabdkacaa"; //"aca"
+        // //     "aacakdbacaa" 
+        // Console.WriteLine(_solution.LongestPalindrome(text));
+        text = "abcdbbfcba"; //"bb"
+        //     "abcfbbdcba" 
+        Console.WriteLine(_solution.LongestPalindrome(text));
+    }
+}
+
+
+public class Solution {
+    public string LongestPalindrome(string s) {
+        if (s.Length == 1) {
+            return s;
+        }
+
+        string temp = string.Join("", s.Reverse());
+        int length = s.Length;
+        int index = 0;
+        string ans = "";
+        while (index != length) {
+            for (int i = index; i <= length; i++) {
+                string tempSub = s[index..i];
+                if (!temp.Contains(tempSub)) continue;
+                if (s[index..i].Length > ans.Length && IsPalindrome(tempSub)) {
+                    ans = s[index..i];
+                }
+            }
+
+            index++;
+        }
+
+        return ans;
+    }
+
+    public bool IsPalindrome(string s) {
+        return s.Equals(string.Join("", s.Reverse()));
+    }
+}
