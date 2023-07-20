d23c474bcfc952a17cb92b788d6839954905810f
ShadowGreg <shadow-greg@yandex.ru>
2023-07-20 21:28:45 +0300

    complit

--- Remove Duplicates from Sorted Array 26/Program.cs
+++ Remove Duplicates from Sorted Array 26/Program.cs
@@ -1,3 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿public class Program {
+    private static Solution _solution;
 
-Console.WriteLine("Hello, World!");
+    public static void Main() {
+        _solution = new Solution();
+
+        int[] num = { 1, 1, 2 };
+        Console.WriteLine(_solution.RemoveDuplicates(num));
+    }
+}
+
+
+public class Solution {
+    public int RemoveDuplicates(int[] nums) {
+        SortedSet<int> temp = new SortedSet<int>(nums);
+        nums = null;
+        nums = temp.ToArray();
+        return temp.Count;
+    }
+}
