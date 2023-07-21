f3db9e44f11c65aaa998cc2794773a01f0831efe
ShadowGreg <shadow-greg@yandex.ru>
2023-07-21 17:25:38 +0300

    add 6 task

--- Zigzag Conversion 6/Program.cs
+++ Zigzag Conversion 6/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System.Data;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
 
 public class Program {
     private static Solution _solution;
@@ -6,44 +8,39 @@ public class Program {
     public static void Main() {
         _solution = new Solution();
 
-        string twxt = "PAYPALISHIRING"; //true
-        Console.WriteLine(_solution.Convert(twxt, 3));
+        string twxt = "PAYPALISHIRING";
+        // Explanation:
+        // PIN   "PINALSIGYAHRPI"
+        // ALSIG
+        // YAHR
+        // PI
+
+        Console.WriteLine(_solution.Convert(twxt, 4));
     }
 }
 
 public class Solution {
     public string Convert(string s, int numRows) {
-        if (s.Length == 1 && numRows == 1 || s.Length < numRows) {
+        if (numRows == 1)
             return s;
-        }
 
-        int charIndex = 0;
-        int сolumnIndex = 0;
-        char[,] zigzag = new char[numRows, s.Length];
+        var result = new StringBuilder();
+        var perIter = numRows * 2 - 2;
+        for (int row = 0; row < numRows; row++) {
+            int i = 0;
+            while ((i + row) < s.Length) {
+                result.Append(s[i + row]);
+                if (row != 0 && row != numRows - 1) {
+                    if ((i + perIter - row) < s.Length) {
+                        result.Append(s[i + perIter - row]);
+                    }
+                }
 
-        while (s.Length != charIndex) {
-            int rowsIndex = 0;
-
-            while (rowsIndex <= numRows) {
-                zigzag[сolumnIndex, rowsIndex] = s[charIndex];
-                rowsIndex++;
-                charIndex++;
-            }
-
-            while (rowsIndex >= 0) {
-                rowsIndex--;
-                сolumnIndex++;
-                charIndex++;
+                i += perIter;
             }
         }
 
-        throw new NotImplementedException();
+//TODO ращобраться в решении алгоритма и реализовать самому.
+        return result.ToString();
     }
-
-
-    // Explanation:
-    // P     I    N   "PINALSIGYAHRPI"
-    // A   L S  I G
-    // Y A   H R
-    // P     I
 }
