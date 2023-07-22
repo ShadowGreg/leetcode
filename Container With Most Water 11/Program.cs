c8becf745d3ec4556293719afae424e75fff93e5
ShadowGreg <shadow-greg@yandex.ru>
2023-07-22 10:15:40 +0300

    end task

--- Container With Most Water 11/Program.cs
+++ Container With Most Water 11/Program.cs
@@ -4,15 +4,28 @@
     public static void Main() {
         _solution = new Solution();
 
-        string haystack = "weqrwerq";
-        string needle = "sad";
-        Console.WriteLine(_solution.StrStr(haystack, needle));
+        int[] height = { 1, 2, 4, 3 };
+        Console.WriteLine(_solution.MaxArea(height));
         Console.WriteLine();
     }
 }
 
 public class Solution {
-    public int StrStr(string haystack, string needle) {
-        return haystack.IndexOf(needle, StringComparison.Ordinal);
+    public int MaxArea(int[] height) {
+        int length = 0,
+            heightLength = height.Length - 1,
+            maxArea = 0;
+        while (length < heightLength) {
+            int currentMax = (heightLength - length) * Math.Min(height[length], height[heightLength]);
+            if (currentMax > maxArea)
+                maxArea = currentMax;
+
+            if (height[length] <= height[heightLength])
+                length++;
+            else
+                heightLength--;
+        }
+
+        return maxArea;
     }
 }
