978e7f53499f5c2f2de564a0233ca7232e47111e
ShadowGreg <shadow-greg@yandex.ru>
2023-09-16 16:06:49 +0300

    готово решение с поиском вхождения в массив

---
+++ Integer to Roman  12/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Integer_to_Roman__12;
+
+public class Program {
+    private static Solution _solution;
+
+    public static void Main() {
+        _solution = new Solution();
+
+        Console.WriteLine(_solution.IntToRoman(3) + "должно быть III  \n Explanation: 3 is represented as 3 ones.");
+        Console.WriteLine(_solution.IntToRoman(58) + "должно быть LVIII\n Explanation: L = 50, V = 5, III = 3.");
+        Console.WriteLine(_solution.IntToRoman(1994) +
+                          "должно быть MCMXCIV \n Explanation: M = 1000, CM = 900, XC = 90 and IV = 4.");
+        Console.WriteLine(_solution.IntToRoman(3) + "должно быть III ");
+    }
+}
+
+public class Solution {
+    public string IntToRoman(int num) {
+        ///TODO доделать
+        return "";
+    }
+}
---
+++ Search Insert Position 35/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Search_Insert_Position_35;
+
+public class Program {
+    private static Solution _solution;
+
+    public static void Main() {
+        _solution = new Solution();
+
+        Console.WriteLine(_solution.SearchInsert(new[] { 1, 3, 5, 6 }, 5) + " должно быть Output: 2");
+        Console.WriteLine(_solution.SearchInsert(new[] { 1, 3, 5, 6 }, 2) + " должно быть Output: 1");
+        Console.WriteLine(_solution.SearchInsert(new[] { 1, 3, 5, 6 }, 7) + " должно быть Output: 4");
+    }
+}
+
+public class Solution {
+    public int SearchInsert(int[] nums, int target) {
+        List<int> numsList = new List<int>(nums);
+        int outIndex = numsList.BinarySearch(target);
+        if (numsList.BinarySearch(target) >= 0) return outIndex;
+        numsList.Add(target);
+        numsList.Sort();
+        outIndex = numsList.BinarySearch(target);
+
+        return outIndex;
+    }
+}
